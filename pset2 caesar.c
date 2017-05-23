cdab379842b02f1cfe402caab3964ca6c3fe1075
Christian Lowe <christglowe@gmail.com>
2017-05-22 22:56:44 -0500

    Create pset2 caesar.c

---
+++ pset2 caesar.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h> 
+#include <stdlib.h>
+
+
+int main (int argc, string argv[]) {
+    
+    
+    int k = atoi(argv[1]);
+     
+    int numargs =  argc;
+    
+    if (numargs != 2) {
+        printf("error please try again \n");
+        return 1; 
+    }
+    
+    
+    printf("plaintext: ");
+    string message = get_string();
+    printf("ciphertext: ");
+    
+    
+    
+    for (int i = 0; i < strlen(message); i++) {
+        
+        char crypted = 0;
+        
+        if (islower(message[i]) && message[i] + k < 123 )
+            {
+                 crypted = message[i] + k
+            } else if ( islower(message[i]))
+                {  crypted = message[i] + (k % 26) - 122;
+                    
+                } else if (isupper(message[i]) && message[i] + k < 91) 
+                    {  crypted = message[i] + k;
+                        
+                        } else if (isupper(message[i])) 
+                            {  crypted = message[i] + (k % 26) - 91; }
+                            
+                            else if (isspace(message[i])) {
+                                crypted = message[i];
+                            }
+                            
+                            else if (isxdigit(message[i])) {
+                                crypted = message[i];
+                            }
+        
+        
+        
+    
+        
+        printf("%c", crypted);
+        
+        
+    }
+    
+    printf("\n"); 
+    
+}
