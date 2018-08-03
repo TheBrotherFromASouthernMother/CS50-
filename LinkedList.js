1bf45f4cca85e6a3fac201b371bf568a98fb6c37
christianlowe <christglowe@gmail.com>
2018-08-02 22:07:24 -0600

    still trying to figure out how to remove a node from the list when there are only two elements

--- LinkedList.js
+++ LinkedList.js
@@ -2,10 +2,11 @@
 
 function LinkedList(){
   this.head = null;
+  this.length = 0;
 }
 
 
-LinkedList.prototype.push = function(val){
+LinkedList.prototype.add = function(val){
   //create memory reference to next node
     var node = {
        value: val,
@@ -18,20 +19,51 @@ LinkedList.prototype.push = function(val){
       this.head = node;
     }
     else {
+      //Starting from the head of the list finds the end of the list and places the current node at it
       current = this.head;
-      //chains the next reference until reaching the null pointer at the end of the list
-      while(current.next){
+      while(current.next !== null){
         current = current.next;
       }
       current.next = node;
     }
+    this.length ++;
   }
 
+LinkedList.prototype.remove = function(val) {
+  let currentNode = this.head;
+  if (!currentNode) {
+    return -1;
+  }
+
+  while(currentNode.next) {
+    if (currentNode.value == val) {
+      console.log("breara")
+      previousNode.next = currentNode.next;
+      this.length --;
+      break;
+    }
+    previousNode = currentNode;
+    currentNode = currentNode.next;
+  }
+
+  if (currentNode.val == val) {
+    previousNode.next = currentNode.next;
+  }
+
+  return this.length;
+
+}
 
 const list = new LinkedList();
 
-list.push(1)
-list.push(2)
+list.add(1)
+list.add(2)
+// list.add(3)
 
 console.log(list.head)
 console.log(list.head.next)
+console.log(list.length)
+
+list.remove(2);
+
+console.log(list)
